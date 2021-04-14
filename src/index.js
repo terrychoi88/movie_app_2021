d03c27c6533b2cbd707acf541ad2d265ddedccf0
Terry <terrychoi88@users.noreply.github.com>
2021-04-14 21:17:41 +0900

    completed

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import App from './App';
-import App_movie from './App_movie';
+import App from './App';
+//import App_movie from './App_movie';
 
 ReactDOM.render(
-  //<App />,
+  <App />,
   // eslint-disable-next-line react/jsx-pascal-case
-  <App_movie />,
+  //<App_movie />,
   document.getElementById('root')
 );
---
+++ src/routes/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './About.css';
+
+function About() {
+    return (
+        <div className="about__container">
+            <span>
+                About this page: I built it.
+            </span>
+        </div>
+    );
+}
+
+export default About;
